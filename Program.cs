c2d37bc8e033e88e4bfcefde6c461cafa02bde7a
Victor Reipur <victor.reipur@gmail.com>
2021-02-09 22:48:45 +0100

    new update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleAccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # ExampleAccessModifiers
-# ExampleAccessModifiers
-# ExampleAccessModifiers
+Fuldt https://www.youtube.com/watch?v=jcn5uCZAk2w
